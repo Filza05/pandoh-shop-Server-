7126cc11451b2f82fbb5221bc1a58e82f547bfdc
Muhammad-Saad0 <doritozz349@gmail.com>
2023-12-27 21:06:53 +0500

    add-product route completed

--- controllers/utils/HelperFunctions.ts
+++ controllers/utils/HelperFunctions.ts
@@ -2,18 +2,21 @@ import { SignUpFormData } from "../../types/types";
 import { getDatabase } from "../../utils/db";
 import bcrypt from "bcrypt";
 import { User, Product } from "../../types/DBTypes";
-import jwt, {Secret} from "jsonwebtoken"
+import jwt, { Secret } from "jsonwebtoken";
 import dotenv from "dotenv";
 import { UserPayload, AddProductFormData } from "../../types/types";
 
-dotenv.config()
+dotenv.config();
 
 const db = getDatabase();
 
-export const checkUsername = async (username: string): Promise<boolean> => {
-  const [result] = await db.query<User[]>("SELECT * FROM users WHERE USERNAME = ?", [
-    username,
-  ]);
+export const checkUsername = async (
+  username: string
+): Promise<boolean> => {
+  const [result] = await db.query<User[]>(
+    "SELECT * FROM users WHERE USERNAME = ?",
+    [username]
+  );
 
   if (result[0]) {
     return true;
@@ -23,9 +26,10 @@ export const checkUsername = async (username: string): Promise<boolean> => {
 };
 
 export const checkUserEmail = async (email: string): Promise<boolean> => {
-  const [result] = await db.query<User[]>("SELECT * FROM users WHERE EMAIL = ?", [
-    email,
-  ]);
+  const [result] = await db.query<User[]>(
+    "SELECT * FROM users WHERE EMAIL = ?",
+    [email]
+  );
 
   if (result[0]) {
     return true;
@@ -58,20 +62,25 @@ export async function comparePassword(
 }
 
 export async function createJWTToken(user: User) {
-  const payload: UserPayload  = {
+  const payload: UserPayload = {
     userid: user.userid,
     username: user.username,
-    email : user.email
-  }
-  const token = jwt.sign(payload, process.env.TOKEN_KEY as string, {expiresIn: '3hr'})
-  return token
+    email: user.email,
+  };
+  const token = jwt.sign(payload, process.env.TOKEN_KEY as string, {
+    expiresIn: "3hr",
+  });
+  return token;
 }
 
-export async function verifyJWTToken (token: string, secretKey: Secret) : Promise<UserPayload | null> {
+export async function verifyJWTToken(
+  token: string,
+  secretKey: Secret
+): Promise<UserPayload | null> {
   return new Promise((resolve, reject) => {
     jwt.verify(token, secretKey, (err, decoded) => {
       if (err) {
-        console.error('Error verifying token:', err);
+        console.error("Error verifying token:", err);
         resolve(null); // Token verification failed
       } else {
         const userPayload = decoded as UserPayload;
@@ -81,14 +90,32 @@ export async function verifyJWTToken (token: string, secretKey: Secret) : Promis
   });
 }
 
-export const checkProductExists = async (productName: string): Promise<boolean> => {
-  const [result] = await db.query<Product[]>("SELECT * FROM products WHERE productname = ?", [
-    productName,
-  ]);
+export const checkProductExists = async (
+  productName: string
+): Promise<boolean> => {
+  const [result] = await db.query<Product[]>(
+    "SELECT * FROM products WHERE productname = ?",
+    [productName]
+  );
 
   if (result[0]) {
     return true;
   } else {
     return false;
   }
-}
+};
+
+export const generateInsertQuery = (
+  insertId: Number,
+  imagesArray: Express.Multer.File[]
+): string => {
+  const query = `INSERT INTO product_images (product_id, imageURL) VALUES
+  ${imagesArray.map((imageObj, index) => {
+    return `(${insertId}, ${imageObj.path})${
+      index == imagesArray.length - 1 ? ";" : ","
+    }`;
+  })}
+  `;
+
+  return query;
+};
